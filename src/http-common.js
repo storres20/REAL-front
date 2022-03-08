936e35b5a2268bb2d67dc44052b85f623b6a1a0b
storres20 <sanchotorres777@gmail.com>
2022-03-08 16:25:06 -0500

    clever cloud remote db

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api",
+  //baseURL: "http://localhost:8080/api",
+  baseURL: "https://apicrudreal.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
