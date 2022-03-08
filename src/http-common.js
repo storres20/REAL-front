c92482438f207661aeb5f4b4118e22917dfff728
storres20 <sanchotorres777@gmail.com>
2022-03-08 16:26:42 -0500

    fix

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   //baseURL: "http://localhost:8080/api",
-  baseURL: "https://apicrudreal.herokuapp.com/api",
+  baseURL: "https://apicrudreal.herokuapp.com/",
   headers: {
     "Content-type": "application/json"
   }
