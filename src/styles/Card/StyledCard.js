aff95f0ede422b403aac171bffa9eb124fbcd1a8
storres20 <sanchotorres777@gmail.com>
2022-04-10 03:22:31 -0500

    add card's style

---
+++ src/styles/Card/StyledCard.js
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+//import { paleteOfColors } from "../theme"
+
+export const StyledCont = styled.div`
+
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+`;
+
+export const StyledDiv = styled.div`
+
+max-width: 400px;
+margin: 15px;
+height: auto;
+height: 80%;
+
+font-size: 1rem;
+`;
+
+export const StyledImg = styled.img`
+
+max-width: 400px;
+margin: 15px;
+height: auto;
+height: 80%;
+
+border-radius: 15px;
+`;
+
+export const StyledP = styled.p`
+
+margin-top: 0;
+`;
