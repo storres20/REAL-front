b9fbadafd618579ab2110d4a5bc3ac7337e23544
storres20 <sanchotorres777@gmail.com>
2022-06-05 10:30:34 -0500

    UPDATED

--- src/http-common.js
+++ src/http-common.js
@@ -4,7 +4,8 @@ export default axios.create({
   //baseURL: "http://localhost:8080/api", //localhost
   //baseURL: "http://localhost:6868/api", //docker
   //baseURL: "https://apicrudreal.herokuapp.com/api",
-  baseURL: "https://real-back.vercel.app/api",
+  //baseURL: "https://real-back.vercel.app/api",
+  baseURL: "https://real-back.herokuapp.com/api",
   
   headers: {
     "Content-type": "application/json"
