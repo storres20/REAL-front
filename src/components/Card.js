992710dff6ec1b904d43128eeeb936e80b222943
storres20 <sanchotorres777@gmail.com>
2022-07-26 12:05:12 -0500

    ADD loading

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import ProductDataService from "../services/ProductService";
 import { Link } from 'react-router-dom';
 
 import {StyledCont, StyledImg, StyledDiv, StyledP} from '../styles/Card/StyledCard';
-
+import '../loading.css'
 
 import noimage from '../noimage.png';
 
@@ -19,15 +19,18 @@ const Product = props => {
     published: false
   };
   const [currentProduct, setCurrentProduct] = useState(initialProductState);
+  const [loading, setLoading] = useState(false) // loading
 
   const getProduct = id => {
     ProductDataService.get(id)
       .then(response => {
         setCurrentProduct(response.data);
         console.log(response.data);
+        setLoading(true) // loading
       })
       .catch(e => {
         console.log(e);
+        setLoading(true) // loading
       });
   };
 
@@ -38,24 +41,36 @@ const Product = props => {
 
   return (
     <div>
+    
+      {loading ? (
+      
+        <StyledCont>
+          <StyledImg src={(currentProduct.image === null || currentProduct.image === "") ? (noimage) : (currentProduct.image)} alt={currentProduct.title} />
+          <StyledDiv>
+            <StyledP><strong>Title:</strong> {currentProduct.title}</StyledP>
+            <p><strong>Description:</strong> {currentProduct.description}</p>
+            <p><strong>Model:</strong> {currentProduct.model}</p>
+            <p><strong>Quantity:</strong> {currentProduct.quantity}</p>
+            <p><strong>Warranty:</strong> {currentProduct.warranty}</p>
+            <p><strong>Price: </strong>$ {currentProduct.price}</p>
+            <Link
+              to={"/"}
+              className="btn btn-danger"
+            >
+              Go Back
+            </Link>
+          </StyledDiv>
+        </StyledCont>
+      
+      ) : (
+      
+        <div className="flexLoad">
+          <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+        </div>
+      
+      )}
 
-      <StyledCont>
-        <StyledImg src={(currentProduct.image === null || currentProduct.image === "") ? (noimage) : (currentProduct.image)} alt={currentProduct.title} />
-        <StyledDiv>
-          <StyledP><strong>Title:</strong> {currentProduct.title}</StyledP>
-          <p><strong>Description:</strong> {currentProduct.description}</p>
-          <p><strong>Model:</strong> {currentProduct.model}</p>
-          <p><strong>Quantity:</strong> {currentProduct.quantity}</p>
-          <p><strong>Warranty:</strong> {currentProduct.warranty}</p>
-          <p><strong>Price: </strong>$ {currentProduct.price}</p>
-          <Link
-            to={"/"}
-            className="btn btn-danger"
-          >
-            Go Back
-          </Link>
-        </StyledDiv>
-      </StyledCont>
+      
 
     </div>
   );
