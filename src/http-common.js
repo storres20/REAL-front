a4a43cb2cb5b6fed9a13e077b1ec57b1ff1266e6
storres20 <sanchotorres777@gmail.com>
2022-12-03 12:22:47 -0500

    Update backend baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -4,8 +4,7 @@ export default axios.create({
   //baseURL: "http://localhost:8080/api", //localhost
   //baseURL: "http://localhost:6868/api", //docker
   //baseURL: "https://apicrudreal.herokuapp.com/api",
-  //baseURL: "https://real-back.vercel.app/api",
-  baseURL: "https://real-back.herokuapp.com/api",
+  baseURL: "https://real-back.vercel.app/api",
   
   headers: {
     "Content-type": "application/json"
